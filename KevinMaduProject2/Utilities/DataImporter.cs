79daf4f16ef2c82926d63a11e232cd651e64998f
km00372 <km00372@CSVLAB-VM13>
2023-11-17 14:36:14 -0500

    Refactor way application reads dictionary
    Hardcode the path of dictionary json file to improve speed of testing program.

--- KevinMaduProject2/Utilities/DataImporter.cs
+++ KevinMaduProject2/Utilities/DataImporter.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using KevinMaduProject2.Model;
+using Microsoft.VisualBasic.Devices;
 using static System.Windows.Forms.Design.AxImporter;
 
 namespace KevinMaduProject2.Utilities
@@ -12,47 +13,61 @@ namespace KevinMaduProject2.Utilities
     public class DataImporter
     {
 
+        private readonly string _filePath;
+        public DataImporter()
+        {
+            _filePath = "C:\\Users\\km00372\\Desktop\\KevinMaduProject2\\KevinMaduProject2\\Data\\dictionary.json";
+        }
+
         private readonly JsonSerializerOptions _options = new()
         {
             PropertyNameCaseInsensitive = true
         };
 
 
+        //public List<Dictionary> ReadFile()
+        //{
+        //    var fileContent = string.Empty;
+        //    var filePath = string.Empty;
+
+        //    using (OpenFileDialog openFileDialog = new OpenFileDialog())
+        //    {
+        //        openFileDialog.Title = "Select a File";
+        //        openFileDialog.InitialDirectory = "c:\\";
+        //        openFileDialog.Filter = "json files (*.json)|*.json";
+        //        openFileDialog.FilterIndex = 2;
+        //        openFileDialog.RestoreDirectory = true;
+
+        //        if (openFileDialog.ShowDialog() == DialogResult.OK)
+        //        {
+        //            filePath = openFileDialog.FileName;
+
+        //            var fileStream = openFileDialog.OpenFile();
+
+        //            using (StreamReader reader = new StreamReader(fileStream))
+        //            {
+        //                fileContent = reader.ReadToEnd();
+        //            }
+        //        }
+        //    }
+
+        //    if (!string.IsNullOrWhiteSpace(fileContent))
+        //    {
+        //        var dictionaries = JsonSerializer.Deserialize<List<Dictionary>>(fileContent, _options)!;
+        //        MessageBox.Show($"Successfully read: {dictionaries.Count} dictionaries");
+        //        return dictionaries;
+
+        //    }
+
+        //    return null;
+        //}
+
         public List<Dictionary> ReadFile()
         {
-            var fileContent = string.Empty;
-            var filePath = string.Empty;
+            var json = File.ReadAllText(_filePath);
+            List<Dictionary> dictionaries = JsonSerializer.Deserialize<List<Dictionary>>(json, _options);
 
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
-            {
-                openFileDialog.Title = "Select a File";
-                openFileDialog.InitialDirectory = "c:\\";
-                openFileDialog.Filter = "json files (*.json)|*.json";
-                openFileDialog.FilterIndex = 2;
-                openFileDialog.RestoreDirectory = true;
-
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    filePath = openFileDialog.FileName;
-
-                    var fileStream = openFileDialog.OpenFile();
-
-                    using (StreamReader reader = new StreamReader(fileStream))
-                    {
-                        fileContent = reader.ReadToEnd();
-                    }
-                }
-            }
-
-            if (!string.IsNullOrWhiteSpace(fileContent))
-            {
-                var dictionaries = JsonSerializer.Deserialize<List<Dictionary>>(fileContent, _options)!;
-                MessageBox.Show($"Successfully read: {dictionaries.Count} dictionaries");
-                return dictionaries;
-
-            }
-
-            return null;
+            return dictionaries;
         }
     }
 }
